53a521de7a188a5091a0a141bf074b6bb483a38c
AturiheihiBlendon <2020blendon@gmail.com>
2023-02-22 21:07:59 +0300

    Task2 - FIFO caching

--- 0x01-caching/main.py
+++ 0x01-caching/main.py
@@ -19,10 +19,26 @@
 # my_cache.print_cache()
 # print(my_cache.get("A"))
 
-""" 1-main """
-FIFOCache = __import__('1-fifo_cache').FIFOCache
+# """ 1-main """
+# FIFOCache = __import__('1-fifo_cache').FIFOCache
 
-my_cache = FIFOCache()
+# my_cache = FIFOCache()
+# my_cache.put("A", "Hello")
+# my_cache.put("B", "World")
+# my_cache.put("C", "Holberton")
+# my_cache.put("D", "School")
+# my_cache.print_cache()
+# my_cache.put("E", "Battery")
+# my_cache.print_cache()
+# my_cache.put("C", "Street")
+# my_cache.print_cache()
+# my_cache.put("F", "Mission")
+# my_cache.print_cache()
+
+""" 2-main """
+LIFOCache = __import__('2-lifo_cache').LIFOCache
+
+my_cache = LIFOCache()
 my_cache.put("A", "Hello")
 my_cache.put("B", "World")
 my_cache.put("C", "Holberton")
@@ -34,4 +50,5 @@ my_cache.put("C", "Street")
 my_cache.print_cache()
 my_cache.put("F", "Mission")
 my_cache.print_cache()
-
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
